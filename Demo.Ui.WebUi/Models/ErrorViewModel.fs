fd4a46d2ce6fb148e6acedddc0f875010bf85815
kgtrue <gammel4@gmail.com>
2022-08-11 20:30:45 +0200

    fix ui proj
    it's now c#

--- Demo.Ui.WebUi/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace Demo.Ui.WebUi.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
