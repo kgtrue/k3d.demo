8f1d7b9721e5a6cab76d951e3b5d75b99791cfd0
kgtrue <gammel4@gmail.com>
2022-08-12 00:40:55 +0200

    can i upgrade cert-mgr

--- ops/step.by.step.setup/setup.cert-manager.ps1
+++ ops/step.by.step.setup/setup.cert-manager.ps1
@@ -1,8 +1,10 @@
 # https://rancher.com/docs/rancher/v2.x/en/installation/k8s-install/helm-rancher/
+# https://cert-manager.io/docs/installation/helm/#1-add-the-helm-repository
 # Install cert-manager with helm
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 kubectl create namespace cert-manager
-helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.0.4 --set installCRDs=true --wait
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.1/cert-manager.crds.yaml
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --version --version v1.9.1 --set installCRDs=true --wait
 kubectl -n cert-manager rollout status deploy/cert-manager
 date
